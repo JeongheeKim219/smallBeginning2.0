91a5e01cdd235c8e57b55c49925a2909d2b89093
Jeonghee Kim <jeongheekim219@gmail.com>
2022-12-18 19:38:06 +0900

    20221218 1) todoState를 checkbox로 변경. 2) todoTable에서 하나의 todo 내용을 수정하다가 다른 todo 수정/ 삭제로 넘어가지 못하도록 수정.

--- src/main/resources/static/js/todoTable.js
+++ src/main/resources/static/js/todoTable.js
@@ -1,82 +1,64 @@
+function setCheckboxByState(todoState){
+    var innerStr = "<input type='checkbox' name='todoState'";
+    innerStr += " value='" + todoState + "'";
+    if (todoState == "1") innerStr += " checked";
+    innerStr += ">";
+
+    return innerStr;
+}
+
 function addTodoListTable(result){
-//    console.log(result);
-//    var resultTodo = result;
+    var table4Todo = document.getElementById("todoListTable");
     var resultTodo = JSON.parse(result);
-    console.log(resultTodo);
-
     if(resultTodo.length > 0){
-        var table4Todo = document.getElementById("todoListsTable");
-
-        var i=0;
         for(var obj of resultTodo){
-
-            todoId = "";
+            var todoId = "";
             if (obj.todoId < 10) {
                 todoId += "0"
             }
-
             todoId += obj.todoId;
+            var todoState = obj.todoState;
 
             var newRaw = table4Todo.insertRow();
             newRaw.id = "td-tr-" + todoId;
             var state = newRaw.insertCell(0);
             var content = newRaw.insertCell(1);
-            var deleteBtn = newRaw.insertCell(2);
-            var editBtn = newRaw.insertCell(3);
+            var editBtn = newRaw.insertCell(2);
+            var deleteBtn = newRaw.insertCell(3);
 
-            state.innerText = obj.todoState;
-            deleteBtn.id = todoId;
             content.classList.add("content");
             deleteBtn.classList.add("delete");
             state.classList.add("state");
             editBtn.classList.add("edit");
-            editBtn.classList.add(i);
 
             content.innerText = obj.todoContent;
-            state.innerText = obj.todoState;
+            state.innerHTML = setCheckboxByState(todoState);
+            state.dataset.state = todoState;
             deleteBtn.innerText = "delete";
-            editBtn.innerText = "  edit";
-            i++;
+            editBtn.innerText = "edit";
         }
     }
 }
 
-function addTodoListTable4edit(result){
-//    var resultTodo = JSON.parse(result);
-    var resultTodo = result;
-    if(resultTodo.length > 0){
-/*        var table4Todo = document.getElementById("todoListsTable");*/
+// 수정
+function editTodoTable(node){
+    var tr = node.parentNode;
+    var content = $(tr).children(".content");
+    var complete = $(tr).children(".edit")
 
-        var i=0;
-        for(var obj of resultTodo){
-            var values = Object.values(obj);
+    complete.attr("class", "complete")
+    complete.text("submit");
 
-            var newRaw = "<tr id='td-tr-" + obj.todoId + "'>";
-            newRaw += "<td id='" + values[5]+ "' class='color'>■</td>" + "<td class='content'>" + values[2] + "</td>";
-            newRaw += "<td class='state'>" + values[4] + "</td>" + "<td class='delete' id='" + values[0] + "'> delete  </td>";
-            newRaw += "<td class='edit' class='" + i + "'>  edit</td></tr>";
+    content.attr("class", "editingContent");
+    content.attr("contentEditable", "true");
+    content.focus();
 
-            $("#todoListsTable").append(newRaw);
-
-            var tester = document.getElementById(values[5]);
-            var colorTodo = "#" + values[5];
-            $(tester).css("color", values[5]);
-            i++;
-        }
-    }
+    return true;
 }
-
-
 
 // 삭제
 function removeTodoTable(){
-    var lengthT = $("#todoListsTable > tbody tr").length;
-    if(lengthT > 0){
-        var todoT = document.getElementById("todoListsTable");
-        for(var i=lengthT-1; i>0; i--){
-           var test = todoT.deleteRow(i);
-        }
-    }
+    $("#todoListTable").html("");
 }
 
 function getDate4Ajax(idStr){
@@ -93,5 +75,6 @@ function getDate4Ajax(idStr){
 
 function checkTodoInTable(trId, result){
     var selectorStr = "#" + trId + " .state";
-    $(selectorStr).text(result);
+    $(selectorStr).attr('data-state', result)
+    $(selectorStr).html(setCheckboxByState(result));
 }
